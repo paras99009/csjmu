0ad7e3b161457e4219e82602b98244428cad739c
Paras Shukla <om127a@gmail.com>
2024-03-24 20:15:41 +0530

    app is launched

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
      <ChakraProvider> 
         <App />
     </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/PARAS_WEB_DUAL/",
   plugins: [react()],
 })
